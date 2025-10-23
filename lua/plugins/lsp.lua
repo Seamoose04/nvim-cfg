f006b1146d793dceb1cfd91e81c523355403134c
seamoose04 <seamusjohnson04@gmail.com>
2025-10-23 01:57:21 +0000

    Fixing lsp which-key

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,18 +1,15 @@
 return {
-  "neovim/nvim-lspconfig",
-	enabled = false,
-  dependencies = { "williamboman/mason.nvim" },
-  config = function()
-		local registry = require("mason-registry")
-
-		local function on_attach(_, bufnr)
-			local opts = { buffer = bufnr, noremap = true, silent = true }
-			vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, opts)
-			vim.keymap.set("n", "<leader>lk", vim.lsp.buf.hover, opts)
-			vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
-			vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, opts)
-		end
-
+	"neovim/nvim-lspconfig",
+	lazy = false,
+	priority = 100,
+	dependencies = { "williamboman/mason.nvim" },
+	keys = {
+		{ "<leader>ld", vim.lsp.buf.definition, desc = "Go to definition" },
+		{ "<leader>lk", vim.lsp.buf.hover, desc = "Show hover info" },
+		{ "<leader>lr", vim.lsp.buf.rename, desc = "Rename symbol" },
+		{ "<leader>la", vim.lsp.buf.code_action, desc = "Code action" },
+	},
+	config = function()
 		-- Server configs go here:
 		local servers = {
 			clangd = {
@@ -40,27 +37,53 @@ return {
 				},
 			},
 		}
-		
+
 		for name, cfg in pairs(servers) do
 			vim.api.nvim_create_autocmd("FileType", {
 				pattern = cfg.filetypes,
 				callback = function()
-					if not registry.is_installed(cfg.pkg) then
-						registry.get_package(cfg.pkg):install()
-					end
-						
-					local pkg = registry.get_package(cfg.pkg)
-					local bin = pkg:get_install_path() .. "/bin/" .. cfg.pkg
+					local mason = require("mason")
+					local registry = require("mason-registry")
+					local mason_path = require("mason.settings").current.install_root_dir
 
-					vim.lsp.start(vim.tbl_extend("force", cfg, {
-						name = name,
-						cmd = { bin, "--stdio" },
-						root_dir = vim.fs.root(0, cfg.root_markers),
-						on_attach = on_attach,
-					}))
+					local function start_lsp()
+						local bin = mason_path .. "/bin/" .. cfg.pkg
+						if vim.fn.executable(bin) == 0 then
+							vim.notify(
+								"LSP binary for " .. cfg.pkg .. " not found at " .. bin ..
+								"\nTry running :Mason and installing it manually.",
+								vim.log.levels.WARN
+							)
+							return
+						end
+
+						vim.lsp.start(vim.tbl_extend("force", cfg, {
+							name = name,
+							cmd = { bin, "--stdio" },
+							root_dir = vim.fs.root(0, cfg.root_markers),
+						}))
+
+						vim.notify("Started LSP: " .. cfg.pkg, vim.log.levels.INFO)
+					end
+
+					-- Get or install package
+					local pkg = registry.get_package(cfg.pkg)
+					if not pkg:is_installed() then
+						vim.notify("Installing missing LSP: " .. cfg.pkg .. " ...", vim.log.levels.INFO)
+						pkg:install()
+
+						-- Wait for install to complete
+						pkg:on("install:success", function()
+							vim.schedule(function()
+								vim.notify("LSP installed: " .. cfg.pkg, vim.log.levels.INFO)
+								start_lsp()
+							end)
+						end)
+					else
+						start_lsp()
+					end
 				end,
 			})
 		end
 	end,
 }
-
